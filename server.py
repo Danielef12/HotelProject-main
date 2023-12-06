7218d11b42f2fb30a2862f66dd370f336e5d7252
danielefiocca <danielef12@hotmail.it>
2023-12-06 19:40:50 +0100

    completamento progetto

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mongoengine import MongoEngine
+from mongoengine.queryset.base import Q
 import smtplib
 
 app = Flask(__name__)
+app.secret_key = "il_mio_segreto"
 
 app.config['MONGODB_SETTINGS'] = {
     'db': 'Merda',
@@ -80,20 +82,36 @@ def prenotazione():
         arrival_date = request.form['arrival-date']
         departure_date = request.form['departure-date']
 
-        prenotazione = Prenotazione(
-            username=username,
-            room_type=room_type,
-            arrival_date=arrival_date,
-            departure_date=departure_date
-        )
-        prenotazione.save()
-        return redirect(url_for('conferma'))
-    return render_template('index.html')
+        print(f"Room Type: {room_type}")
+        print(f"Arrival Date: {arrival_date}")
+        print(f"Departure Date: {departure_date}")
+        # Verifica se la camera è già prenotata per le date specificate
+        existing_booking = Prenotazione.objects(
+            Q(room_type=room_type) &
+            ((Q(arrival_date__lte=arrival_date) & Q(departure_date__gte=arrival_date)) |
+             (Q(arrival_date__lte=departure_date) & Q(departure_date__gte=departure_date)))).first()
+        print(f"Existing Booking: {existing_booking}")
+
+        if existing_booking:
+            flash('Camera già prenotata per le date specificate. Scegliere altre date o un altro tipo di camera.',
+                  'error')
+            return render_template('prenotazione.html')
+        else:
+
+            prenotazione = Prenotazione(
+                username=username,
+                room_type=room_type,
+                arrival_date=arrival_date,
+                departure_date=departure_date
+            )
+            prenotazione.save()
+            return redirect(url_for('conferma', username=username, room_type=room_type,
+                                    arrival_date=arrival_date, departure_date=departure_date))
+        return render_template('index.html')
 
 
 @app.route('/conferma', methods=['GET', 'POST'])
 def conferma():
-
     username = request.args.get('username')
     room_type = request.args.get('room_type')
     arrival_date = request.args.get('arrival_date')
